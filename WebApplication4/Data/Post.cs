3115d4cc53081bd990b1057000447fcddc4b21b1
Joakim Jansson <joakimxjansson@gmail.com>
2025-03-05 11:08:46 +0100

    Name change

--- WebApplication4/Data/Post.cs
+++ WebApplication4/Data/Post.cs
@@ -2,7 +2,7 @@ namespace WebApplication4.Data;
 
 public class Post {
     public int PostId { get; set; }
-    public User UserId { get; set; }
+    public User User { get; set; }
     public string Title { get; set; }
     public string Content { get; set; }
     public DateTime PublishDate { get; set; }
--- WebApplication4/Data/Review.cs
+++ WebApplication4/Data/Review.cs
@@ -3,7 +3,7 @@ namespace WebApplication4.Data;
 public class Review {
     public int ReviewId { get; set; }
     public User UserId { get; set; }
-    public Product ProdId { get; set; }
+    public Product Product { get; set; }
     public string Content { get; set; }
     public DateTime Date { get; set; }
 }
