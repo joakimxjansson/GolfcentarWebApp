5143f74d6bede3ce1ceb71d5b379b42afd237671
Joakim Jansson <joakimxjansson@gmail.com>
2025-03-05 11:14:01 +0100

    namechange and deleted migrationer

--- WebApplication4/Data/CartItems.cs
+++ WebApplication4/Data/CartItems.cs
@@ -2,7 +2,7 @@ namespace WebApplication4.Data;
 
 public class CartItems {
     public int CartItemsId { get; set; }
-    public Product Prod { get; set; }
+    public Product Product { get; set; }
     public int Quantity { get; set; }
     public int TotalPrice { get; set; }
 }
--- WebApplication4/Data/Review.cs
+++ WebApplication4/Data/Review.cs
@@ -2,7 +2,7 @@ namespace WebApplication4.Data;
 
 public class Review {
     public int ReviewId { get; set; }
-    public User UserId { get; set; }
+    public User User { get; set; }
     public Product Product { get; set; }
     public string Content { get; set; }
     public DateTime Date { get; set; }
