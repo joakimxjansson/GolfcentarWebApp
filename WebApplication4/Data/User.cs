19c6aa29ef43da19637149ba5db62d02772d8450
Joakim Jansson <joakimxjansson@gmail.com>
2025-03-11 10:12:03 +0100

    Uppdaterad login/registrering, samt ändrat User table i DB Efter denna uppdatering måste alla köra db upate igen (OBS INGEN MIGRATIONS KRÄVS)

--- WebApplication4/Data/User.cs
+++ WebApplication4/Data/User.cs
@@ -1,13 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace WebApplication4.Data;
 
 public class User {
     public int UserId { get; set; }
+    [Required]
     public string Username { get; set; }
+    [Required]
     public string Password { get; set; }
+    [Required]
     public string Email { get; set; }
+    [Required]
     public string FirstName { get; set; }
+    [Required]
     public string LastName { get; set; }
-    public int Admin { get; set; }
-    public int Saldo { get; set; }
-    public string UserImage { get; set; }
+
+    public int Admin { get; set; } = 0;
+    public int Saldo { get; set; } = 1000;
+    public string? UserImage { get; set; }
 }
--- WebApplication4/Migrations/GolfContextModelSnapshot.cs
+++ WebApplication4/Migrations/GolfContextModelSnapshot.cs
@@ -138,12 +138,11 @@ namespace WebApplication4.Migrations
 
                     b.ToTable("Product");
 
-
                     b.HasData(
                         new
                         {
                             ProductId = 1,
-                            ProdDescription = "Fin driver av högsta kvalité",
+                            ProdDescription = "Fin driver av h�gsta kvalit�",
                             ProdImage = "driver.jpg",
                             ProdName = "Golfklubba Driver",
                             ProdPrice = 2599m
@@ -151,9 +150,9 @@ namespace WebApplication4.Migrations
                         new
                         {
                             ProductId = 2,
-                            ProdDescription = "Järnklubba i världsklass",
+                            ProdDescription = "J�rnklubba i v�rldsklass",
                             ProdImage = "jarnklubba.jpg",
-                            ProdName = "Golfklubba Järn",
+                            ProdName = "Golfklubba J�rn",
                             ProdPrice = 1999m
                         },
                         new
@@ -167,7 +166,7 @@ namespace WebApplication4.Migrations
                         new
                         {
                             ProductId = 4,
-                            ProdDescription = "Vattentålig",
+                            ProdDescription = "Vattent�lig",
                             ProdImage = "golfbag.jpg",
                             ProdName = "Golfbag",
                             ProdPrice = 1999m
@@ -183,7 +182,7 @@ namespace WebApplication4.Migrations
                         new
                         {
                             ProductId = 6,
-                            ProdDescription = "Högkvalitativa bollar.",
+                            ProdDescription = "H�gkvalitativa bollar.",
                             ProdImage = "golfballs.jpg",
                             ProdName = "Golfbollar (12-pack)",
                             ProdPrice = 349m
@@ -191,7 +190,7 @@ namespace WebApplication4.Migrations
                         new
                         {
                             ProductId = 7,
-                            ProdDescription = "Perfekt när solen tittar fram",
+                            ProdDescription = "Perfekt n�r solen tittar fram",
                             ProdImage = "keps.jpg",
                             ProdName = "Golfkeps",
                             ProdPrice = 249m
@@ -199,13 +198,11 @@ namespace WebApplication4.Migrations
                         new
                         {
                             ProductId = 8,
-                            ProdDescription = "pegs i trä",
+                            ProdDescription = "pegs i tr�",
                             ProdImage = "peg.jpg",
                             ProdName = "peg (10-pack)",
                             ProdPrice = 39m
                         });
-
-
                 });
 
             modelBuilder.Entity("WebApplication4.Data.Review", b =>
@@ -294,7 +291,6 @@ namespace WebApplication4.Migrations
                         .HasColumnType("int");
 
                     b.Property<string>("UserImage")
-                        .IsRequired()
                         .HasColumnType("nvarchar(max)");
 
                     b.Property<string>("Username")
@@ -303,25 +299,18 @@ namespace WebApplication4.Migrations
 
                     b.HasKey("UserId");
 
-                    b.ToTable("User");
+                    b.ToTable("Users");
                 });
 
             modelBuilder.Entity("WebApplication4.Data.CartItems", b =>
                 {
-
-
                     b.HasOne("WebApplication4.Data.Product", "Product")
-
-
                         .WithMany()
                         .HasForeignKey("ProductId")
                         .OnDelete(DeleteBehavior.Cascade)
                         .IsRequired();
 
-
                     b.Navigation("Product");
-
-
                 });
 
             modelBuilder.Entity("WebApplication4.Data.Order", b =>
