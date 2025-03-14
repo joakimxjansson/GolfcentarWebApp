5af1827e37ce826727213696e540dd67e172bb3b
Joakim Jansson <joakimxjansson@gmail.com>
2025-03-14 01:31:29 +0100

    Lagt till edit customers,

--- WebApplication4/Pages/Admin/EditCustomers.cshtml.cs
+++ WebApplication4/Pages/Admin/EditCustomers.cshtml.cs
@@ -1,49 +1,27 @@
 using System.Net;
-using System.Net.Mime;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using WebApplication4.Data;
-using WebApplication4.Services;
-
 namespace WebApplication4.Pages.Admin;
 
 public class EditCustomers : PageModel {
     private readonly GolfContext _context;
-    private readonly UserService _userService;
 
-    public EditCustomers(GolfContext context ,UserService userService) {
+    public EditCustomers(GolfContext context) {
         _context = context;
-        _userService = userService;
     }
     public IEnumerable<User>? Users { get; set; } = new List<User>();
     [BindProperty]
     public User? User { get; set; } 
-    [BindProperty]
-    public IFormFile ImageFile { get; set; } 
-    
    
     
-    public IActionResult OnGet() {
-        
-        var id = HttpContext.Session.GetInt32("Id");
-        if (id == null) {
-            return RedirectToPage("/Login");
-        }
-        var role = _userService.GetRole(id.Value);
-        if (role == 0) {
-            return RedirectToPage("/MyProfile");
-        }
+    public void OnGet() {
         Users = _context.Users;
-        return Page();
 
     }
 
     public async Task <IActionResult> OnPostUpdateAsync(int id) {
-        
-        
         var user = await _context.Users.FindAsync(id);
-    
-
         if (user == null) {
             return NotFound();
         }
@@ -53,18 +31,6 @@ public class EditCustomers : PageModel {
             user.LastName = User.LastName;
             user.Email = User.Email;
             user.Saldo = User.Saldo;
-            if (ImageFile != null) {
-            var fileName = Guid.NewGuid().ToString() + Path.GetExtension(ImageFile.FileName);
-            var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/images", fileName);
-            using (var stream = new FileStream(filePath, FileMode.Create))
-            {
-                await ImageFile.CopyToAsync(stream);
-            }
-
-           
-            user.UserImage = "/images/" + fileName;
-        
-            }
             await _context.SaveChangesAsync();
             return RedirectToPage("/Admin/EditCustomers");
         }
