e956fed91a9215c334907489a8c21ee9f7750c1a
Batool Fahmi <batool.fahmi.webd24jon@edu.tucsweden.se>
2025-03-16 16:08:04 +0100

    La till blogginlägg filer

---
+++ WebApplication4/Views/Blog/Blog.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Blogginlägg.Views.Forum
+{
+    public class PostDetailsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
