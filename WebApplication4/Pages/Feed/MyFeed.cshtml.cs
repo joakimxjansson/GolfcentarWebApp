f89476ee6f252bd7b1aa4bdf9b23b75d72b0efec
An2nJohansson <an2n_johansson@outlook.com>
2025-04-01 00:32:49 +0200

    Lagt till ny mapp för feed, skapat sidan MyFeed, raderade migrations och la in mina migrationer i gitignore

---
+++ WebApplication4/Pages/Feed/MyFeed.cshtml.cs
@@ -0,0 +1,40 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using System.Xml.Linq;
+using WebApplication4.Data;
+using WebApplication4.Services;
+
+namespace WebApplication4.Pages.Feed
+{
+    public class MyFeedModel : PageModel
+    {
+        private readonly GolfContext _context;
+        
+        public MyFeedModel(GolfContext context)
+        {
+            _context = context;
+        }
+
+        public List<Post> GetPosts { get; set; } = new List<Post>();
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            GetPosts = await _context.Post.OrderByDescending(p => p.PublishDate)
+            .Include(p => p.User).ToListAsync();
+            return Page();
+        }
+
+        
+
+        public IActionResult OnPostMyFeed()
+        {
+            return RedirectToPage("/Feed/MyFeed");
+        }
+
+        public IActionResult OnPostFollowing()
+        {
+            return RedirectToPage("/Feed/Following");
+        }
+    }
+}
