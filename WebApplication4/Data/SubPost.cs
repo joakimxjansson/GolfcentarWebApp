d4563403e6f22534834a8637736fb3e54e673901
Joakim Jansson <joakimxjansson@gmail.com>
2025-04-01 20:53:27 +0200

    Fixade Comment och Forum, påbörjat med följare, lagt till defaultImage om man inte har profilbild

--- WebApplication4/Data/SubPost.cs
+++ WebApplication4/Data/SubPost.cs
@@ -1,8 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace WebApplication4.Data;
 
 public class SubPost {
     public int SubPostId { get; set; }
     public Post Post { get; set; }
+    [Required]
+    public int PostId { get; set; }
+    [Required]
+    public int UserId { get; set; }
+    [Required]
     public string Comment { get; set; }
     public DateTime Date { get; set; }
 }
