305d8118b13588440095f252d434511cebc0a2cd
Joakim Jansson <joakimxjansson@gmail.com>
2025-04-03 22:40:22 +0200

    La till en "Logga in knapp" ifall man är inne på findpeople, och försöker följa

--- WebApplication4/Pages/Admin/EditOrders.cshtml.cs
+++ WebApplication4/Pages/Admin/EditOrders.cshtml.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
+using PdfSharp.Drawing;
+using PdfSharp.Pdf;
 using WebApplication4.Data;
 using WebApplication4.Services;
 
@@ -48,7 +50,28 @@ public class EditOrders : PageModel {
 
 
 
-    public IActionResult OnPostDownload() {
+    public IActionResult OnPostDownload(string id) {
+        var order = _context.Order.Include(p => p.User)
+            .Include(o => o.Product)
+            .Where(o => o.OrderNumber == id).FirstOrDefault();
+        using (PdfDocument document = new PdfDocument()) {
+            document.PageLayout = PdfPageLayout.SinglePage;
+            document.Info.Title = "Kvitto: " + order.OrderNumber;
+            PdfPage page = document.AddPage();
+            XGraphics gfx = XGraphics.FromPdfPage(page);
+            XFont font = new XFont("Verdana", 12);
+            gfx.DrawString("Kvitto från Golfcentar AB", font, XBrushes.Black, new XPoint(200, 20 ));
+
+            gfx.DrawString("Köpare: " + order.User.FirstName + " " + order.User.LastName + " Totalpris: " + order.TotalPrice.ToString() +
+               " Produkt(er): " + order.Product.ProdName, font, XBrushes.Black, new XPoint(40, 40 ));
+            gfx.DrawString("Köpare: " + order.User.FirstName + " " + order.User.LastName + " Totalpris: " + order.TotalPrice.ToString() +
+                         " Produkt(er): " + order.Product.ProdName, font, XBrushes.Black, new XPoint(40, 60 ));
+           
+            using (MemoryStream ms = new MemoryStream()) {
+                document.Save(ms);
+                return File(ms.ToArray(), "application/pdf");
+            }
+        }
         Orders = _context.Order.Include(o => o.User).Include(o => o.Product).ToList();
         return Page();
         
