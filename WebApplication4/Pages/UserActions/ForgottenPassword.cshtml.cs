ff161ed68c1f19bb0b22e0afa0e30e85eb6bb521
Olle Frisk <olle.frisk.webd24jon@edu.tucsweden.se>
2025-04-03 22:30:27 +0200

    Created new files and navigation to pages where you can get the username of a profile, and reset the password.

---
+++ WebApplication4/Pages/UserActions/ForgottenPassword.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication4.Pages.UserActions
+{
+    public class ForgottenPasswordModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
